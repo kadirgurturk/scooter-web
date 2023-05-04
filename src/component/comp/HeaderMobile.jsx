189ca7e6395f9ceac4245b95677cb383ca2a687b
kadirgurturk <89667179+kadirgurturk@users.noreply.github.com>
2023-05-04 14:53:59 +0300

    responsive 2

---
+++ src/component/comp/HeaderMobile.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+
+export const HeaderMobile = () =>{
+    const [isClick, setClick] = useState(false)
+
+    return(
+        <nav className="header">
+            <div>
+                
+            </div>
+
+        </nav>
+    )
+}
